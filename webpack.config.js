f57326d1c557d654c22e911ff4bd286987b84d7a
Matt <mwbertolucci@gmail.com>
2023-02-21 15:27:57 -0300

    Set css-loader and local dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    devServer: {
+        static: './dist',
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
@@ -12,5 +16,13 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
 };
